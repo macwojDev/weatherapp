c0d52f4e12a7020740b2ac8bb6e432293bc870b6
macwojDev <92087346+macwojDev@users.noreply.github.com>
2023-05-10 15:58:52 +0200

    małe poprawki scrapingu

--- weatherapp/core/views.py
+++ weatherapp/core/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
-from requests import Session,get
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# przydatne komercyjnie, otwieranie chrome w trybie niewidocznym.
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+
+driver = webdriver.Chrome(options=chrome_options)
 
 
-
-
-
-driver = webdriver.Chrome()
 def get_html_content(city):
     url = f'https://www.google.com/search?q=weather+in+{city}'
 
-    USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-    LANGUAGE = "en-US,en;q=0.5"
-    session = Session()
-#   pobieranie danych z sekcji header kodu html
-    session.headers['User-Agent'] = USER_AGENT
-    session.headers['Accept-Language'] = LANGUAGE   
-    session.headers['Content-Language'] = LANGUAGE
-    
 #   formatowanie parametru, aby wskazywał na poprawny adres url przy miastach o kilkuczłonowej nazwie
     city = city.replace(' ','+')
 
@@ -25,7 +19,6 @@ def get_html_content(city):
     driver.get(url)
     accept_button = driver.find_element(by='id', value='L2AGLb')
     accept_button.click()
-    return driver
 
 def index(request):
     if 'city' in request.GET:
