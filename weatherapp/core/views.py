5b98c9c567371ccd8609dcfdf549b7d20e6adc72
macwojDev <maciekwojdyna1@gmail.com>
2023-05-14 20:34:06 +0200

    przechwycanie danych i pokazywanie ich w html

--- weatherapp/core/views.py
+++ weatherapp/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
 # przydatne komercyjnie, otwieranie chrome w trybie niewidocznym.
 chrome_options = Options()
@@ -8,6 +9,10 @@ chrome_options.add_argument('--headless')
 
 driver = webdriver.Chrome(options=chrome_options)
 
+url = 'https://www.google.pl'
+driver.get(url)
+accept_button = driver.find_element(by='id', value='L2AGLb')
+accept_button.click()
 
 def get_html_content(city):
     url = f'https://www.google.com/search?q=weather+in+{city}'
@@ -17,21 +22,25 @@ def get_html_content(city):
 
 #   pobieranie kodu html tej strony dzięki .text, bez .text pobiera response http
     driver.get(url)
-    accept_button = driver.find_element(by='id', value='L2AGLb')
-    accept_button.click()
 
 def index(request):
+    context={}
     if 'city' in request.GET:
-        from bs4 import BeautifulSoup
         city = request.GET.get('city')
         get_html_content(city)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         region = soup.find('span', attrs={'class':'BBwThe'}).text
         temperatura = soup.find('span', attrs={'id':'wob_tm'}).text
-        print(temperatura)
-        print(region)
-        context = {'region' : region,
-                   'temperatura' : temperatura
-                   }
+        wilgotność = soup.find('span', attrs={'id':'wob_hm'}).text
+        opady = soup.find('span', attrs={'id':'wob_pp'}).text
+        wiatr = soup.find('span', attrs={'id':'wob_ws'}).text
+        context = {
+                'city':city,
+                'region' : region,
+                'temperatura' : temperatura,
+                'wilgotnosc' : wilgotność,
+                'opady':opady,
+                'wiatr':wiatr
+                }
     return render(request, 'core/index.html', context)
 
